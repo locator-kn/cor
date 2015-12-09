40e1622f05f1ace7808442bdb4b85e031a13f693
Timo Weiß <info@timo-weiss.com>
2015-12-09 18:22:31 +0100

    add pact for promises

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Bluebird = require('bluebird');
+
+// plugins
+const Chairo = require('chairo');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -16,6 +20,18 @@ server.connection({
 
 // Add the route
 server.route(user.routes);
+
+
+// register plugins
+
+server.register({ register: Chairo }, err => {
+    //console.log(server.seneca.act);
+    //
+    let act = Bluebird.promisify(server.seneca.act, server.seneca);
+    //server.expose('pact', act);
+
+    server.decorate('request', 'pact', act);
+});
 
 // Start the server
 server.start((err) => {
