32e337ff227517342389c621ec0fff9eced3a8d8
Timo Weiß <info@timo-weiss.com>
2015-12-09 18:03:56 +0100

    update impl

--- handler/userHandler.js
+++ handler/userHandler.js
@@ -1,9 +1,25 @@
 'use strict';
-
+const hoek = require('hoek');
 let handler = {};
+const basicPin = {
+    role: 'user'
+};
 
 handler.postUser = (request, reply) => {
-    reply('hallo');
+    // decorate basic pin with cmd
+    let requestPattern = {cmd: 'login'};
+
+    // add data to request pattern
+    requestPattern.data = request.payload;
+
+    // merge request patter with basic pin
+    requestPattern = hoek.merge(requestPattern, basicPin);
+
+    request.server.pact(requestPattern, (err, data) => {
+
+            reply(data);
+        });
+
 };
 
 
