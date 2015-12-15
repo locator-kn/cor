d03c6ed63ea387bde08069cb0701e59b34cbc987
Timo Weiß <info@timo-weiss.com>
2015-12-16 00:12:48 +0100

    add defaults to make the devs lifes easier :shit: :bowtie:

--- validation/locationValidation.js
+++ validation/locationValidation.js
@@ -3,15 +3,15 @@ const Joi = require('joi');
 let validations = {};
 
 validations.nearbyQuery = Joi.object().keys({
-    long: Joi.number().required(),
-    lat: Joi.number().required(),
-    maxDistance: Joi.number(),
-    limit: Joi.number()
+    long: Joi.number().required().default(9.169753789901733),
+    lat: Joi.number().required().default(47.66868204997508),
+    maxDistance: Joi.number().default(2),
+    limit: Joi.number().default(20)
 });
 
 validations.postSchoenhier = Joi.object().keys({
-    long: Joi.number().required(),
-    lat: Joi.number().required()
+    long: Joi.number().required().default(9.169753789901733),
+    lat: Joi.number().required().default(47.66868204997508)
 });
 
 module.exports = validations;
