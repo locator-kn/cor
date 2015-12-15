8d27f509ce796b2d2561c02090663f07f1ba94a0
Timo Weiß <info@timo-weiss.com>
2015-12-16 00:58:15 +0100

    add getLocationById

--- handler/locationHandler.js
+++ handler/locationHandler.js
@@ -8,6 +8,24 @@ const basicPin = {
     role: 'location'
 };
 
+handler.getLocationById = (request, reply) => {
+    // decorate basic pin with cmd
+    let requestPattern = {cmd: 'locationById'};
+
+    // add data to request pattern
+    requestPattern.data = request.query;
+
+    // merge request patter with basic pin
+    hoek.merge(requestPattern, basicPin);
+
+    request.server.pact(requestPattern)
+        .then(reply)
+        .catch(error => {
+            reply(boom.badRequest(error));
+        });
+
+};
+
 handler.getLocationsNearby = (request, reply) => {
     // decorate basic pin with cmd
     let requestPattern = {cmd: 'nearby'};
