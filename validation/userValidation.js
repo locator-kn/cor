20b716334461917a21e0206b6cc3091871cffd04
NicolaiLoeffler <nicolai.loeffler92@gmail.com>
2015-12-15 19:07:59 +0100

    add register validation

--- validation/userValidation.js
+++ validation/userValidation.js
@@ -7,4 +7,11 @@ validations.postUser = Joi.object().keys({
     password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/)
 });
 
+validations.register = Joi.object().keys({
+    mail: Joi.string().email().min(3).max(60).required()
+        .description('Mail address'),
+    password: Joi.string().regex(/[a-zA-Z0-9@#$%_&!"§\/\(\)=\?\^]{3,30}/).required()
+        .description('User set password')
+});
+
 module.exports = validations;
