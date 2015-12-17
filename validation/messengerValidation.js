3c852deaefa2882d678e34b940f86d607ea490cc
Timo Weiß <info@timo-weiss.com>
2015-12-17 16:13:04 +0100

    add dataPaged

--- validation/messengerValidation.js
+++ validation/messengerValidation.js
@@ -3,8 +3,14 @@ const Joi = require('joi');
 
 let validations = {};
 
+
 validations.conversationId = Joi.object().keys({
     conversationId: Joi.string().required()
+});
+
+validations.dataPaged = Joi.object().keys({
+    page: Joi.number().default(0),
+    elements: Joi.number().default(20)
 });
 
 module.exports = validations;
