523d7d55425ff741691db4a83dfa0426b7a563eb
Timo Weiß <info@timo-weiss.com>
2015-12-20 18:39:59 +0100

    add impl for postConversation

--- handler/messengerHandler.js
+++ handler/messengerHandler.js
@@ -24,6 +24,32 @@ handler.getMessagesByConversationId = (request, reply) => {
     return reply(boom.notImplemented('todo'));
 };
 
+
+handler.postConversation = (request, reply) => {
+
+    let conversationData = request.payload;
+
+    conversationData.participants.push({
+        user_id: util.getUserId(request.auth),
+        last_read: Date.now()
+    });
+
+    let senecaAct = util.setupSenecaPattern({
+        cmd: 'newconversation'
+    }, conversationData, basicPin);
+
+    request.server.pact(senecaAct)
+        .then(reply)
+        .catch(error => {
+            if (error.cause.name === 'ValidationError') {
+                return reply(boom.badRequest(error.details.message));
+            }
+            console.log(JSON.stringify(error));
+            return reply(boom.badRequest(error.details.message));
+
+        });
+};
+
 handler.postMessage = (request, reply) => {
 
     let messageData = request.payload;
