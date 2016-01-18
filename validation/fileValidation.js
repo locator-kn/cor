bd655198d237bea8917ebda6fb614b51f500c475
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-01-18 20:46:02 +0100

    add validation for various types of files (TODO)

--- validation/fileValidation.js
+++ validation/fileValidation.js
@@ -8,4 +8,28 @@ validations.getFile = Joi.object().keys({
     ext: Joi.string().required()
 });
 
+
+
+validations.getAudioFile = Joi.object().keys({
+    fileId: Joi.string().required(),
+    name: Joi.string().required(),
+    ext: Joi.string().required()
+});
+
+
+
+validations.getImageFile = Joi.object().keys({
+    fileId: Joi.string().required(),
+    name: Joi.string().required(),
+    ext: Joi.string().required()
+});
+
+
+
+validations.getVideoFile = Joi.object().keys({
+    fileId: Joi.string().required(),
+    name: Joi.string().required(),
+    ext: Joi.string().required()
+});
+
 module.exports = validations;
