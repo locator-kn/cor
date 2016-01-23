b360e809d00fdcede2e773c811a85310c8f757ec
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-01-23 21:06:16 +0100

    implement handler for registration of device

--- handler/deviceHandler.js
+++ handler/deviceHandler.js
@@ -1,15 +1,32 @@
 'use strict';
 const boom = require('boom');
 
-//const util = require('../lib/util');
+const util = require('../lib/util');
+const slack = require('ms-utilities').slack;
 
 let handler = {};
-/*const basicPin = {
- role: 'device'
- };*/
+const basicPin = {
+    role: 'device'
+};
 
 handler.register = (request, reply) => {
-    return reply(boom.notImplemented('Wait for it'));
+
+    let senecaAct = util.setupSenecaPattern('register', request.payload, basicPin);
+
+    request.server.pact(senecaAct)
+        .then(result => {
+
+            return reply({message: 'device registered, locator-cookie was set'})
+                .state('locator_device', result.sessionData).code(201);
+        })
+        .catch(err => {
+
+            reply(boom.badRequest(err));
+
+            slack.sendSlackError(process.env['SLACK_ERROR_CHANNEL'], 'Error registering device:');
+            slack.sendSlackError(process.env['SLACK_ERROR_CHANNEL'], err);
+        });
+
 };
 
 module.exports = handler;
