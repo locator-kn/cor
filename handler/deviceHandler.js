1dfc141b1bbe26f2574834df8ba4428ce1ee5b78
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-01-23 16:27:49 +0100

    add device handler file

---
+++ handler/deviceHandler.js
@@ -0,0 +1,14 @@
+'use strict';
+const boom = require('boom');
+
+//const util = require('../lib/util');
+
+let handler = {};
+/*const basicPin = {
+ role: 'device'
+ };*/
+
+handler.register = (request, reply) => {
+    return reply(boom.notImplemented('Wait for it'));
+};
+
