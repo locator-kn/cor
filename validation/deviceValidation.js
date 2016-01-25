ae460b336e1969ea3c5e6d3798b43fb8f9a9af3f
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-01-25 17:27:16 +0100

    add validation for devices and push information

--- validation/deviceValidation.js
+++ validation/deviceValidation.js
@@ -2,4 +2,16 @@
 const Joi = require('joi');
 let validations = {};
 
-validations.register = Joi.any();
+validations.register = Joi.object().keys({
+    deviceId: Joi.string().required(),
+    type: Joi.string().required().valid(['ios', 'android']),
+    version: Joi.string().required(),
+    deviceModel: Joi.string().required()
+});
+
+validations.pushInfo = Joi.object().keys({
+    pushToken: Joi.string().required(),
+    version: Joi.string().required()
+});
+
+module.exports = validations;
