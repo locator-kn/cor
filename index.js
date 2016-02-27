5d24bea8f664a88b22d61708cf0206f113164ab3
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-02-27 14:14:40 +0100

    add remaining microservices

--- index.js
+++ index.js
@@ -337,6 +337,8 @@ Glue.compose(manifest, {relativeTo: __dirname}, (err, server) => {
         //.use(process.env['SENECA_TRANSPORT_METHOD'] + '-transport')
         // announce a microservice with pin and transport type the service is listening to
         .client({type: 'tcp', port: 7003, host: 'localhost', pin: 'role:messenger,cmd:*'})
+        .client({type: 'tcp', port: 7005, host: 'localhost', pin: 'role:mailer,cmd:*'})
+        .client({type: 'tcp', port: 7004, host: 'localhost', pin: 'role:notifications,cmd:*'})
         .client({type: 'tcp', port: 7002, host: 'localhost', pin: 'role:user,cmd:*'})
         .client({type: 'tcp', port: 7001, host: 'localhost', pin: 'role:location,cmd:*'})
         .client({type: 'tcp', port: 7010, host: 'localhost', pin: 'role:reporter,cmd:*'});
