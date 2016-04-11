c3085561d02e83c9e7650e304ddde11805c0e2a5
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-04-11 17:49:42 +0200

    Revert "update glue and refactor server setup"
    This reverts commit f16d8e86fd6e2c45e736a78d7c25e7c4d8f09e0d.

--- index.js
+++ index.js
@@ -28,145 +28,158 @@ let manifest = {
         //host: process.env['API_HOST'] || 'localhost',
         port: process.env['API_PORT'] || 8000
     }],
-    registrations: [
-        {plugin: 'inert'}, // server side rendering (for swagger)
-        {plugin: 'vision'}, // server side rendering (for swagger)
-        {plugin: 'hapi-swagger'}, // documentation of API
-        {plugin: 'chairo'},// plugin for the microservice framework seneca
-        {plugin: 'hapi-auth-cookie'}, // cookie plugin for authentication
-        {plugin: 'h2o2'}, // proxy plugin (used for ms-fileserve)
-        {
-            plugin: {
-                register: 'good',
-                options: {
-                    requestPayload: true,
-                    reporters: [{
-                        reporter: require('good-console'),
-                        events: {log: '*', response: '*', request: '*'}
-                    }, {
-                        reporter: require('good-bunyan'),
-                        config: {
-                            logger: require('bunyan')
-                                .createLogger({
-                                    name: 'locator',
-                                    streams: [{
-                                        type: 'rotating-file',
-                                        path: process.env['PATH_LOGFILE_COR'] + 'cor.log',
-                                        period: '1d',   // daily rotation
-                                        count: 14        // keep 14 back copies
-                                    }]
-                                })
-                                .child({service: 'cor'}),
-                            levels: {
-                                log: 'info',
-                                response: 'info',
-                                request: 'info'
-                            }
-                        },
-                        events: {log: '*', response: '*', request: '*'}
-                    }]
-                }
+    plugins: [{
+        // plugin for the microservice framework seneca
+        'chairo': {}
+    }, {
+        // server side rendering
+        'inert': {}
+    }, {
+        // server side rendering
+        'vision': {}
+    }, {
+        // documentation of API
+        'hapi-swagger': {}
+    }, {
+        // cookie plugin for authentication
+        'hapi-auth-cookie': {}
+    }, {
+        // proxy plugin
+        'h2o2': {}
+    }, {
+        // Logger plugin
+        'good': [{
+            options: {
+                requestPayload: true,
+                reporters: [{
+                    reporter: require('good-console'),
+                    events: {log: '*', response: '*', request: '*'}
+                }, {
+                    reporter: require('good-bunyan'),
+                    config: {
+                        logger: require('bunyan')
+                            .createLogger({
+                                name: 'locator',
+                                streams: [{
+                                    type: 'rotating-file',
+                                    path: process.env['PATH_LOGFILE_COR'] + 'cor.log',
+                                    period: '1d',   // daily rotation
+                                    count: 14        // keep 14 back copies
+                                }]
+                            })
+                            .child({service: 'cor'}),
+                        levels: {
+                            log: 'info',
+                            response: 'info',
+                            request: 'info'
+                        }
+                    },
+                    events: {log: '*', response: '*', request: '*'}
+                }]
             }
-        }
-
-    ]
+        }]
+    }]
 };
+
 // compose Server with plugins
-Glue.compose(manifest, {relativeTo: __dirname})
-    .then(server => {
+Glue.compose(manifest, {relativeTo: __dirname}, (err, server) => {
 
-        // Add the API routes
-        server.route(user.routes);
-        server.route(location.routes);
-        server.route(device.routes);
+    if (err) {
+        throw err;
+    }
 
-        // Add develop/test routes only if not in production
-        if (process.env['NODE_ENV'] !== 'production') {
-            server.route(develop.routes);
+    // configure auth strategy
+    server.auth.strategy('session', 'cookie', 'optional', {
+        password: process.env['COOKIE_SECRET'],
+        ttl: 24 * 60 * 60 * 1000 * 365,   // 1 year
+        keepAlive: true,
+        cookie: 'locator_session',
+        isSecure: false, //TODO
+        clearInvalid: true
+    });
+
+
+    // configure device cookie
+    server.state('locator', {
+        ttl: 24 * 60 * 60 * 1000 * 365,   // 1 year
+        isSecure: false,
+        path: '/',
+        encoding: 'base64json'
+        // TODO: set password and inspect API
+    });
+
+    // decorate request object with user id and device id
+    server.ext('onPostAuth', (request, reply) => {
+
+        request.basicSenecaPattern = {
+            requesting_user_id: util.getUserId(request.auth),
+            requesting_device_id: util.getDeviceId(request.state),
+            cmd: ''
+        };
+
+
+        reply.continue();
+    });
+
+    // Add the API routes
+    server.route(user.routes);
+    server.route(location.routes);
+    server.route(device.routes);
+
+    // Add develop/test routes only if not in production
+    if (process.env['NODE_ENV'] !== 'production') {
+        server.route(develop.routes);
+    }
+    
+    server.on('request-error', (request, err) => {
+
+        // log 500 code
+        log.fatal('Server Error', {
+            error: err,
+            requestData: request.orig,
+            path: request.path
+        });
+
+    });
+
+
+    // log errors before response is sent back to user
+    server.ext('onPreResponse', (request, reply) => {
+        const response = request.response;
+        if (!response.isBoom) {
+            return reply.continue();
         }
 
-        // configure auth strategy
-        server.auth.strategy('session', 'cookie', 'optional', {
-            password: process.env['COOKIE_SECRET'],
-            ttl: 24 * 60 * 60 * 1000 * 365,   // 1 year
-            keepAlive: true,
-            cookie: 'locator_session',
-            isSecure: false, //TODO
-            clearInvalid: true
-        });
+        // log joi validation error
+        if (response.output.statusCode === 400) {
 
-
-        // configure device cookie
-        server.state('locator', {
-            ttl: 24 * 60 * 60 * 1000 * 365,   // 1 year
-            isSecure: false,
-            path: '/',
-            encoding: 'base64json'
-            // TODO: set password and inspect API
-        });
-
-        // decorate request object with user id and device id
-        server.ext('onPostAuth', (request, reply) => {
-
-            request.basicSenecaPattern = {
-                requesting_user_id: util.getUserId(request.auth),
-                requesting_device_id: util.getDeviceId(request.state),
-                cmd: ''
-            };
-            reply.continue();
-        });
-
-        // logging 
-        server.on('request-error', (request, err) => {
-
-            // log 500 code
-            log.fatal('Server Error', {
-                error: err,
+            log.fatal('Client error', {
+                response: response,
                 requestData: request.orig,
                 path: request.path
             });
+        }
 
-        });
-
-
-        // log errors before response is sent back to user
-        server.ext('onPreResponse', (request, reply) => {
-            const response = request.response;
-            if (!response.isBoom) {
-                return reply.continue();
-            }
-
-            // log joi validation error
-            if (response.output.statusCode === 400) {
-
-                log.fatal('Client error', {
-                    response: response,
-                    requestData: request.orig,
-                    path: request.path
-                });
-            }
-            reply.continue();
-        });
-
-
-        // configure seneca
-        server.seneca.use('mesh', {auto: true, pin: {role: 'cor'}});
-
-        // promisify seneca.act
-        let pact = Bluebird.promisify(server.seneca.act, {context: server.seneca});
-        // decorate server object with promisified seneca.act
-        server.decorate('server', 'pact', pact);
-
-        // start the server
-        return server.start(err => {
-
-            if (err) {
-                throw err;
-            }
-            log.info('Server running at:', server.info.uri)
-        });
-    })
-    .catch(err => {
-        log.fatal({err: err});
-        throw err;
+        reply.continue();
     });
+
+
+    // configure seneca
+    server.seneca.use('mesh', {auto: true, pin: {role: 'cor'}});
+
+    // promisify seneca.act
+    let pact = Bluebird.promisify(server.seneca.act, {context: server.seneca});
+    // decorate server object with promisified seneca.act
+    server.decorate('server', 'pact', pact);
+
+    // start the server
+    server.start(err => {
+
+        if (err) {
+            throw err;
+        }
+        log.info('Server running at:', server.info.uri);
+    });
+
+
+});
